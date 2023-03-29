37d29660016257c3c20eb9798cd7554be2c5e458
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2023-03-29 10:01:51 +0300

    On car-http.service.ts create CRUD

--- src/app/api/services/car-http.service.ts
+++ src/app/api/services/car-http.service.ts
@@ -5,13 +5,30 @@ import {Car} from "../models/Car";
 
 
 @Injectable({providedIn: 'root'})
-export class CarHttpService{
+export class CarHttpService {
   private readonly URL = `${API_URL}/cars`;
 
   constructor(private http: HttpClient) {
   }
 
-  getAll(){
-    return this.http.get<Car[]>(this.URL);
+  getAll() {
+    return this.http.get<Car[]>(`${this.URL}`);
   }
+
+  get(id: number) {
+    return this.http.get<Car>(`${this.URL}/${id}`);
+  }
+
+  create(car: Car) {
+    return this.http.post(`${this.URL}`, car);
+  }
+
+  update(car: Car) {
+    return this.http.put(`${this.URL}/${car.id}`, car);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.URL}/${id}`);
+  }
+
 }
